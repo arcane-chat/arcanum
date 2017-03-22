9c92f0b7f9cfcdc259f98f35169492717bae00b5
Remy Goldschmidt <taktoa@gmail.com>
2017-03-22 17:02:34 -0500

    fixed some environment variable issues

--- arcanum.nix
+++ arcanum.nix
@@ -5,6 +5,15 @@
 stdenv.mkDerivation {
   name = "arcanum";
   src = ./.;
+
   buildInputs = [ qmakeHook makeQtWrapper qtbase qtwebengine qtwebchannel ];
+
+  postInstall = ''
+      wrapQtProgram "$out/bin/arcanum" \
+          --set "NIX_PROFILES" "" \
+          --set "QTWEBENGINEPROCESS_PATH" "${qtwebengine.out}/libexec"
+  '';
+
+  NIX_PROFILES = "";
   QTWEBENGINEPROCESS_PATH = "${qtwebengine.out}/libexec";
 }
--- default.nix
+++ default.nix
@@ -1,5 +1,7 @@
+{ nixpkgs ? <nixpkgs> }:
+
 let
-  pkgs = import <nixpkgs> { config = {}; };
+  pkgs = import nixpkgs { config = {}; };
   arcanum = pkgs.qt56.callPackage ./arcanum.nix {};
 in {
   inherit arcanum;
